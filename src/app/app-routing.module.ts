e1e5276ffebbc66b4a979215c3fe1580192659bb
nbm6d3tp <79254818+nbm6d3tp@users.noreply.github.com>
2024-06-16 16:17:07 +0200

    Set up structure of project + Install necessary libraries

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,34 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MyEventsComponent } from './my-events/my-events.component';
+import { AllEventsComponent } from './all-events/all-events.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { MyProfileComponent } from './my-profile/my-profile.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'my-events',
+    component: MyEventsComponent,
+  },
+  {
+    path: 'all-events',
+    component: AllEventsComponent,
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
+  {
+    path: 'register',
+    component: RegisterComponent,
+  },
+  { path: 'my-profile', component: MyProfileComponent },
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { MyEventsComponent } from './my-events/my-events.component';
+import { AllEventsComponent } from './all-events/all-events.component';
+import { MyProfileComponent } from './my-profile/my-profile.component';
+import { RegisterComponent } from './register/register.component';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    MyEventsComponent,
+    AllEventsComponent,
+    MyProfileComponent,
+    RegisterComponent,
+    HeaderComponent,
+    FooterComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  imports: [BrowserModule, AppRoutingModule],
+  providers: [provideAnimationsAsync()],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
